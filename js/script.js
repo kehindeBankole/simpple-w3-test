e4ac4fcaadcc0a3d70bd7c8e84d400f69f58a951
kehindeBankole <33335391+kehindeBankole@users.noreply.github.com>
2018-07-03 12:14:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+    function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+    
+    
+function myMap()
+{
+  myCenter=new google.maps.LatLng(41.878114, -87.629798);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
+ 
+
+
+
+
+
